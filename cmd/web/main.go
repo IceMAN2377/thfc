e2efc35e7899bdfe33e9df34d302d292a457e414
IceMAN2377 <alisher.karmenbayev@nu.edu.kz>
2025-08-20 16:44:17 +0500

    with struct dependency

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,100 +1,41 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/IceMAN2377/thfc/internal/models"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
-var psql *sqlx.DB
-
 func main() {
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	var err error
 
 	connStr := "postgres://postgres:secret@localhost:5432/thfc?sslmode=disable"
 
-	psql, err = sqlx.Open("postgres", connStr)
+	db, err := sqlx.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal("Failed to prepare DB")
+		logger.Error("Failed to prepare DB")
 	}
 
-	err = psql.Ping()
-	if err != nil {
-		log.Fatal("Failed to connect to DB")
+	if err = db.Ping(); err != nil {
+		logger.Error("Failed to connect DB")
 	}
 
 	fmt.Println("Success to DB")
+
+	app := models.NewRecordHandler(db)
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("POST /texts/", postText)
-	mux.HandleFunc("GET /texts/{title}", GetByTitle)
+	mux.HandleFunc("POST /texts/", app.PostText)
+	mux.HandleFunc("GET /texts/{title}", app.GetByTitle)
 
 	log.Print("Starting")
 	log.Fatal(http.ListenAndServe(":8080", mux))
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("HOMEPAGE"))
-}
-
-func postText(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Database connection status: %v", psql)
-	var record models.Record
-
-	if err := json.NewDecoder(r.Body).Decode(&record); err != nil {
-		log.Printf("some error: %v", err)
-		http.Error(w, "Invalid JSON", 400)
-		return
-	}
-
-	if record.Title == "" || record.Content == "" {
-		http.Error(w, "Empty input", 400)
-		return
-	}
-
-	stmt, err := psql.Preparex(`INSERT INTO records (title, content) VALUES ($1, $2)`)
-	if err != nil {
-		log.Printf("DB error: %v", err)
-		http.Error(w, "Internal server error", 500)
-		return
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(record.Title, record.Content)
-	if err != nil {
-		log.Printf("some error: %v", err)
-		http.Error(w, "error", 500)
-		return
-	}
-
-	w.Header().Set("Content-type", "application/json; charset=utf-8")
-	w.WriteHeader(201)
-	json.NewEncoder(w).Encode(map[string]string{"status": "created"})
-}
-
-func GetByTitle(w http.ResponseWriter, r *http.Request) {
-	title := r.PathValue("title")
-
-	stmt, err := psql.Preparex(`SELECT title, content FROM records WHERE title=$1`)
-	if err != nil {
-		log.Printf("Error preparing stmt:%v", err)
-		http.Error(w, "Internal error", http.StatusInternalServerError)
-		return
-	}
-
-	var record models.Record
-
-	if err := stmt.Get(&record, title); err != nil {
-		log.Printf("error retrieving the record:%v", err)
-		http.Error(w, "Internal error", http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-type", "application/json; charset=utf-8")
-	json.NewEncoder(w).Encode(record)
-
 }
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,6 +1,79 @@
 package models
 
+import (
+	"encoding/json"
+	"github.com/jmoiron/sqlx"
+	"log"
+	"net/http"
+)
+
 type Record struct {
 	Title   string `json:"title" db:"title"`
 	Content string `json:"content" db:"content"`
+}
+
+type RecordHandler struct {
+	db *sqlx.DB
+}
+
+func NewRecordHandler(db *sqlx.DB) *RecordHandler {
+	return &RecordHandler{
+		db: db,
+	}
+}
+
+func (h *RecordHandler) PostText(w http.ResponseWriter, r *http.Request) {
+	var record Record
+
+	if err := json.NewDecoder(r.Body).Decode(&record); err != nil {
+		log.Printf("some error: %v", err)
+		http.Error(w, "Invalid JSON", 400)
+		return
+	}
+
+	if record.Title == "" || record.Content == "" {
+		http.Error(w, "Empty input", 400)
+		return
+	}
+
+	stmt, err := h.db.Preparex(`INSERT INTO records (title, content) VALUES ($1, $2)`)
+	if err != nil {
+		log.Printf("DB error: %v", err)
+		http.Error(w, "Internal server error", 500)
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(record.Title, record.Content)
+	if err != nil {
+		log.Printf("some error: %v", err)
+		http.Error(w, "error", 500)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json; charset=utf-8")
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(map[string]string{"status": "created"})
+}
+
+func (h *RecordHandler) GetByTitle(w http.ResponseWriter, r *http.Request) {
+	title := r.PathValue("title")
+
+	stmt, err := h.db.Preparex(`SELECT title, content FROM records WHERE title=$1`)
+	if err != nil {
+		log.Printf("Error preparing stmt:%v", err)
+		http.Error(w, "Internal error", http.StatusInternalServerError)
+		return
+	}
+
+	var record Record
+
+	if err := stmt.Get(&record, title); err != nil {
+		log.Printf("error retrieving the record:%v", err)
+		http.Error(w, "Internal error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(record)
+
 }
